4e0c75042d6d5a601db9d430be662565ced34e2d
Etsybt <hmidouchnouhaila2@gmail.com>
2023-11-28 13:26:55 +0000

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,59 +1,62 @@
 #include <stddef.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_perfect - Check if the tree is NULL
+ * @tree: Pointer to the root node of the tree
+ * Return: checker
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    // Check if the tree is NULL
-    if (tree == NULL)
-        return 0;
+	size_t height;
 
-    // Calculate the height of the tree
-    size_t height = binary_tree_height(tree);
+	if (tree == NULL)
+		return (0);
 
-    // Check if the tree is perfect
-    return is_perfect_recursive(tree, height, 0);
+	height = binary_tree_height(tree);
+
+	return (is_perfect_recursive(tree, height, 0));
 }
 
 /**
  * binary_tree_height - Calculate the height of a binary tree
  * @tree: Pointer to the root node of the tree
- *
  * Return: Height of the tree, or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	size_t left_height, right_height;
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+	if (tree == NULL)
+		return (0);
 
-    return (left_height > right_height ? left_height : right_height) + 1;
+	left_height = binary_tree_height(tree->left);
+
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
- * is_perfect_recursive - Helper function to check if a tree is perfect recursively
+ * is_perfect_recursive - Helper function to check
+ * if a tree is perfect recursively
  * @tree: Pointer to the node being checked
  * @height: Height of the tree
  * @level: Current level of the node
- *
  * Return: 1 if the tree is perfect, 0 otherwise
  */
-int is_perfect_recursive(const binary_tree_t *tree, size_t height, size_t level)
+int is_perfect_recursive(const binary_tree_t *tree,
+		size_t height, size_t level)
 {
-    // Check if the tree is NULL
-    if (tree == NULL)
-        return 1;
+	if (tree == NULL)
+		return (1);
 
-    // Check if the current node is a leaf node
-    if (tree->left == NULL && tree->right == NULL)
-        return (height == level + 1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (height == level + 1);
 
-    // Check if the current node has both left and right children
-    if (tree->left == NULL || tree->right == NULL)
-        return 0;
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
 
-    // Recursively check the left and right subtrees
-    return is_perfect_recursive(tree->left, height, level + 1) &&
-           is_perfect_recursive(tree->right, height, level + 1);
+	return (is_perfect_recursive(tree->left, height, level + 1) &&
+		is_perfect_recursive(tree->right, height, level + 1));
 }
