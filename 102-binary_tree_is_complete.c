9e5382ccacc1ba221d9591dec6e6dba108297b6d
Etsybt <hmidouchnouhaila2@gmail.com>
2023-11-28 15:33:50 +0000

    update

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: pointer to the root nide of the tree to check
+ * Return: checker
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+
+}
