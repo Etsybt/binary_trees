6faeaa99d64c2920c5e763bc47900f576914cf7b
Etsybt <hmidouchnouhaila2@gmail.com>
2023-11-28 12:58:05 +0000

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,14 +11,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        left = binary_tree_height(tree->left);
+	left = binary_tree_height(tree->left);
 
-        right = binary_tree_height(tree->right);;
+	right = binary_tree_height(tree->right);
 
-        return (1 + ((left > right) ? left : right));
+	return (1 + ((left > right) ? left : right));
 
 }
 /**
@@ -34,6 +34,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	left = binary_tree_height(tree->left);
+
 	right = binary_tree_height(tree->right);
 
 	return ((int)(left - right));
