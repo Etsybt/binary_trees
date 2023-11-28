ba9d4527ad0a8f948677d639205f38bf1b1f2ff6
Etsybt <hmidouchnouhaila2@gmail.com>
2023-11-28 13:31:07 +0000

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,8 @@
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: a pointer to the node to insert the left-child in
  * @value: the value to store in the new node
- * Return:  a pointer to the created node, or NULL on failure or if parent is NULL
+ * Return:  a pointer to the created node
+ * or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -29,5 +30,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	parent->left = new_node;
 
 	return (new_node);
-
 }
